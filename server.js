927a9f66202e11d311172285f9a224d35858e6e8
Gavin Carter <gavinnoelcarter@gmail.com>
2020-05-11 00:37:42 -0700

    updated server

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ const express = require('express')
 const cors = require('cors');
 const connectDB = require('./config/db')
 const app = express();
+const path = require('path')
+
 app.use(cors())
 connectDB();
 
 // init middleware
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => {
-  // res.json({ msg: 'welcome to the api' })
-  res.send('hello')
-  console.log('hello')
-})
+// app.get('/', (req, res) => {
+//   // res.json({ msg: 'welcome to the api' })
+//   res.send('hello')
+//   console.log('hello')
+// })
 
 
 //Import routes
@@ -21,7 +23,13 @@ app.use('/api/users', require('./server/routes/users'))
 app.use('/api/auth', require('./server/routes/auth'))
 app.use('/api/contacts', require('./server/routes/contacts'))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+} //heroku postbuild
 
 const PORT = process.env.PORT || 5000;
 
